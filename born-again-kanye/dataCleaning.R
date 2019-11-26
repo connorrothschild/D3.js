e8a1dcb7996d8494564fd98d7c503007f5df07cf
Connor Rothschild <connorrothschild@gmail.com>
2019-11-26 13:18:32 -0600

    clean repository

--- born-again-kanye/dataCleaning.R
+++ born-again-kanye/dataCleaning.R
@@ -7,7 +7,6 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = 'x')
 access_token <- get_spotify_access_token()
 
 kanye <- get_artist_audio_features('kanye west')
-kanye
 
 grouped <- kanye %>% 
   group_by(album_name) %>% 
@@ -18,3 +17,9 @@ grouped <- grouped %>%
   mutate(album_name = ifelse(album_name == "Kanye West Presents Good Music Cruel Summer", "Good Music Cruel Summer", album_name))
 
 write.csv(grouped, "./kanye_data.csv")
+
+long_kanye_data <- tidyr::pivot_longer(grouped, danceability:tempo, "group") %>% 
+  rename(score = "value")
+
+write.csv(long_kanye_data, "./long_kanye_data.csv")
+
